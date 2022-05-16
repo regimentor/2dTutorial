ecec550f0737cec7c77790d27564755a398e1f1f
Evgeny Shchekoldin <regimentor@yandex.ru>
2022-05-16 11:28:49 +0300

    refactor moving between waypoints

---
+++ Assets/Scripts/MovingBetweenWaypoints.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMoving : MonoBehaviour
+{
+
+    [SerializeField] GameObject start;
+    [SerializeField] GameObject end;
+    [SerializeField] float speed = 1f;
+
+    private int direction = -1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    void FixedUpdate()
+    {
+
+
+        if (direction < 0)
+            if (Vector2.Distance(start.transform.position, transform.position) > 0.1f)
+                transform.position = Vector2.MoveTowards(transform.position, start.transform.position, speed * Time.fixedDeltaTime);                
+            else
+                direction = 1;
+        else   
+            if (Vector2.Distance(end.transform.position, transform.position) > 0.1f)          
+                transform.position = Vector2.MoveTowards(transform.position, end.transform.position, speed * Time.fixedDeltaTime);           
+            else          
+                direction = -1;
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
